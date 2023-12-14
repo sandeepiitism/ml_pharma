4807935d523fcb507ec088ac8b5a6346702e7915
Sandeep <sandeepiit.ism@gmail.com>
2023-12-14 19:18:34 +0530

    ingestion.py created

--- src/logger.py
+++ src/logger.py
@@ -15,5 +15,5 @@ logging.basicConfig(
 )
 
 ### ------- to test log -------------- ###
-if __name__ == "__main__":
-    logging.info("logging has started")
+# if __name__ == "__main__":
+#     logging.info("logging has started")
